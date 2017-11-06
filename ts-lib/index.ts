323307ba1c880847b0ab1558db5077f0260cebd2
Chris Witt <rillist@gmail.com>
2017-11-05 20:24:53 -0500

    commented out oldLogic and references new table class, and player/playeroptions

--- ts-lib/index.ts
+++ ts-lib/index.ts
@@ -1,7 +1,15 @@
-import oldLogic = require('../lib/logic')
+// import oldLogic = require('../lib/logic');
 
-export var helloWorld = function(hello:string = 'hello', world:string = 'world'){
+export const helloWorld = (hello = 'hello', world = 'world') => {
 	return `${hello} ${world}!`;
 };
 
-export var newTable = oldLogic.newTable;
+// export let newTable = oldLogic.newTable;
+export { Table } from './table';
+export { Player, PlayerOptions } from './player';
+
+// todo: decouple modules, by accepting params for other models?
+// todo: make events consistent, const for names, respons models, etc.
+// todo: add support for antes
+// todo: add support for no-limit and limit rules
+// todo: add support for tournament
